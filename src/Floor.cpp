5ec67f826d2c8b51b2958a67ae7b403ac1c4a2f7
ZiadMabrouk <ziadmabrouk@cmail.carleton.ca>
2025-01-31 16:48:58 -0500

    Update Floor.cpp

--- src/Floor.cpp
+++ src/Floor.cpp
@@ -12,21 +12,3 @@
 #include "Scheduler.h"
 
 
-template <typename Type> class Floor
-{
-private:
-    std::string name;
-    Scheduler<Type>& scheduler;
-
-public:
-    Floor( Scheduler<Type>& a_scheduler ) : name(), scheduler(a_scheduler) {}
-
-    void operator()( const std::string& name ) {
-        for(int i = 0; i < 10; i++) {
-            std::cout << name << "(" << std::this_thread::get_id() << ") produced " << i << std::endl;
-            scheduler.put( i );
-            std::cout << name << "(" << std::this_thread::get_id() << ") put in box " << i << std::endl;
-            std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
-        }
-    }
-};
