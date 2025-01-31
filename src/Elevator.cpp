bdee702b5fd94f4ddecf61331ead4962545e821c
ZiadMabrouk <ziadmabrouk@cmail.carleton.ca>
2025-01-31 16:48:42 -0500

    Update Elevator.cpp

--- src/Elevator.cpp
+++ src/Elevator.cpp
@@ -13,11 +13,4 @@
 
 #include "Scheduler.h"
 
-void Elevator<>::operator()(const std::string &name) {
-    for(int i = 0; i < 10; i++) {
-        //std::cout << name << "(" << std::this_thread::get_id() << ") ready to consume " << i << std::endl;
-        //Type item = scheduler.get();
-        //std::cout << name << "(" << std::this_thread::get_id() << ") consumed " << item << std::endl;
-       // std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
-    }
-}
+
