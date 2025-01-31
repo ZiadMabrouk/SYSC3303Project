167d14070ae8f2c8167e82a7614ae342833791a4
ZiadMabrouk <ziadmabrouk@cmail.carleton.ca>
2025-01-31 16:47:56 -0500

    Update Elevator.h

--- src/Elevator.h
+++ src/Elevator.h
@@ -4,23 +4,9 @@
 
 #ifndef ELEVATOR_H
 #define ELEVATOR_H
-#include <iostream>
-#include <chrono>
-#include <thread>
-#include <mutex>
+
 #include "Scheduler.h"
 
-
-template <typename Type> class Elevator
-{
-private:
-    std::string name;
-    Scheduler<Type>& scheduler;
-
-public:
-    Elevator( Scheduler<Type>& a_scheduler ) : name(), scheduler(a_scheduler) {}
-    void operator()( const std::string& name ) {}
-};
 
 
 #endif //ELEVATOR_H
