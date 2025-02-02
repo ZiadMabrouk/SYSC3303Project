1f274b68d95b74ba55206cf373a6ab41f0a54aa2
ZiadMabrouk <ziadmabrouk@cmail.carleton.ca>
2025-02-01 20:55:20 -0500

    Update Scheduler.cpp

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -125,3 +125,39 @@ e_struct Scheduler::get() {
 void Scheduler::operator()() {
     while (1) {}
 }
+
+
+int main(void) {
+
+    Scheduler scheduler; 
+// Test struct 1
+    ElevatorData test1;
+    test1.datetime = {0, 30, 14, 1, 0, 125}; // 2025-01-01 14:30:00
+    test1.floor_number = 1;
+    test1.floor_up_button = true;
+    test1.floor_down_button = false;
+
+    // Test struct 2
+    ElevatorData test2;
+    test2.datetime = {500, 45, 16, 1, 1, 125}; // 2025-02-01 16:45:00.500
+    test2.floor_number = 5;
+    test2.floor_up_button = false;
+    test2.floor_down_button = true;
+
+    // Test struct 3
+    ElevatorData test3;
+    test3.datetime = {750, 15, 9, 1, 2, 125}; // 2025-03-01 09:15:00.750
+    test3.floor_number = 10;
+    test3.floor_up_button = true;
+    test3.floor_down_button = false;
+
+    scheduler.put(test1)
+    scheduler.put(test2)
+    scheduler.put(test3)
+
+    e_struct outtest1 = scheduler.get();
+    e_struct outtest2 =scheduler.get();
+    e_struct outtest3 = scheduler.get();
+    std::cout << "Success"; 
+    return 1;
+}
