f1dd85ac5cedb1591231f39b9a0d59237e9a3065
ZiadM101 <ziadmabrouk1@gmail.com>
2025-02-19 08:45:16 -0500

    initial

---
+++ cmake-build-debug/CMakeFiles/SYSC3303Project.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SYSC3303Project.dir/src/Elevator.cpp.obj"
+  "CMakeFiles/SYSC3303Project.dir/src/Elevator.cpp.obj.d"
+  "CMakeFiles/SYSC3303Project.dir/src/Floor.cpp.obj"
+  "CMakeFiles/SYSC3303Project.dir/src/Floor.cpp.obj.d"
+  "CMakeFiles/SYSC3303Project.dir/src/Scheduler.cpp.obj"
+  "CMakeFiles/SYSC3303Project.dir/src/Scheduler.cpp.obj.d"
+  "CMakeFiles/SYSC3303Project.dir/src/main.cpp.obj"
+  "CMakeFiles/SYSC3303Project.dir/src/main.cpp.obj.d"
+  "SYSC3303Project.exe"
+  "SYSC3303Project.exe.manifest"
+  "SYSC3303Project.pdb"
+  "libSYSC3303Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SYSC3303Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
